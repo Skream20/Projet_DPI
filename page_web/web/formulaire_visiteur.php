9ae69d23bffce0982fe0557bac3ca1ee66a3b4ec
Skream20 <118281774+Skream20@users.noreply.github.com>
2024-01-25 15:45:19 +0100

    update_yann

--- page_web/web/formulaire_visiteur.php
+++ page_web/web/formulaire_visiteur.php
@@ -5,19 +5,32 @@ include 'db_connect.php';
 $visiteurBD= connexion();
 
 
-$ETP = $GET_["TXTETP"];
-$km = $GET_["TXTkilo"];
-$repas = $GET_["TXTrep"];
-$nuit = $GET_["Txthot"];
+$id = $GET_["TxTID"];
+$nom = $GET_["TxTNom"];
+$prenom = $GET_["TxTprenom"];
+$adress = $GET_["TxTadresse"];
+$ville = $GET_["TxTville"];
+$CP = $GET_["TxTcp"];
+$date_emb = $GET_["embauche"];
+$login = $GET_["login"];  
+$mdp =$GET_["TxTmdp"];
+
+function visiteur($id,$nom,$prenom,$adress,$ville,$CP,$date_emb,$login,$mdp){
+    while (True){
+        $sql = "INSERT INTO visiteur(VIS_ID,VIS_PRENOM,VIS_NOM,VIS_ADRESSE,VIS_CP,VIS_VILLE,VIS_DATE_EMBAUCHE) VALUES ('$id','$nom','');";
+
+    }
 
 
 
-if (isset($_GET['BOvalider'])) {
-
-    calul_prix($km, $repas, $nuit);
 }
 
-$sql = "INSERT INTO etudiant(nom, prenom) VALUES ('$TabloNomFamille[$n]', '$TabloPrenom[$p]');";
+
+if (isset($_GET['submit'])) {
+
+    visiteur($id,$nom,$nom,$prenom,$adress,$ville,$CP,$date_emb,$login,$mdp);
+}
+
 
 
 echo "Sql : " . $sql . "<br />";
@@ -26,6 +39,9 @@ $result = $visiteurBD->query($sql)
 
 // Fermer la connexion MYSQL
 $visiteurBD->close();
+
+
+
 
 
 
