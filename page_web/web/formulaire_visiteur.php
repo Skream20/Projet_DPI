54457b25f2141cf1f2872b396a7f21bef010ec54
skream20 <118281774+Skream20@users.noreply.github.com>
2024-02-08 15:17:31 +0100

    update_yann

--- page_web/web/formulaire_visiteur.php
+++ page_web/web/formulaire_visiteur.php
@@ -13,30 +13,59 @@ $CP = $_GET["TxTcp"];
 $date_emb = $_GET["TxTembauche"];
 $login = $_GET["TxTlogin"];
 $mdp = $_GET["TxTmdp"];
+$date = horloge();
 
-function visiteur($visiteurBD, $id, $nom, $prenom, $adress, $ville, $CP, $date_emb, $login, $mdp)
+function horloge()
+{
+    $datejour = new DateTime();
+    $Joursemaine = $datejour->format("l");
+    $joursmois = $datejour->format("d");
+    $mois = $datejour->format("F");
+    $annee = $datejour->format("Y");
+    $heure = $datejour->format("H:i");
+
+    echo "Le $Joursemaine $joursmois $mois $annee à $heure";
+}
+
+
+
+#enregistre user
+
+function visiteur($visiteurBD, $id, $nom, $prenom, $adress, $ville, $CP, $date_emb, $login, $mdp, $date)
 {
     // Préparation de la requête SQL (ajustez les colonnes et les valeurs en conséquence)
-    $sql = "INSERT INTO visiteur(VIS_ID, VIS_PRENOM, VIS_NOM, VIS_ADRESSE, VIS_CP, VIS_VILLE, VIS_DATE_EMBAUCHE, VIS_ID_login, VIS_ID_password) 
-            VALUES ('$id', '$prenom', '$nom', '$adress', '$CP', '$ville', '$date_emb', '$login', '$mdp')";
+    $sql = "INSERT INTO visiteur(VIS_ID, VIS_PRENOM, VIS_NOM, VIS_ADRESSE, VIS_CP, VIS_VILLE, VIS_DATE_EMBAUCHE) 
+            VALUES ('$id', '$prenom', '$nom', '$adress', '$CP', '$ville', '$date_emb')
+            ";
+    $sql2 = "INSERT INTO USER(VIS_ID, login,password,dteConexion)values('$id', '$login','$mdp','$date')";
 
     echo "Sql : " . $sql . "<br />";
+    echo "sql :" . $sql2 . "<br/>";
 
     // Exécution de la requête
     $result = $visiteurBD->query($sql);
+    $result2 =  $visiteurBD->query($sql2);
 
     if ($result === TRUE) {
+        echo "Enregistrement ajouté avec succès.<br/>";
+        #echo "Voulez vous login?"
+        #if 
+    } else {
+        echo "Erreur lors de l'ajout de l'enregistrement : " . $visiteurBD->error . "<br/>";
+    }
+
+    if ($result2 === TRUE) {
         echo "Enregistrement ajouté avec succès.";
         #echo "Voulez vous login?"
         #if 
     } else {
-        echo "Erreur lors de l'ajout de l'enregistrement : " . $visiteurBD->error;
+        echo "Erreur lors de l'ajout de l'enregistrement : " . $visiteurBD->error . "<br/>";
     }
 }
 
 if (isset($_GET['submit'])) {
     while (true) {
-        visiteur($visiteurBD, $id, $nom, $prenom, $adress, $ville, $CP, $date_emb, $login, $mdp);
+        visiteur($visiteurBD, $id, $nom, $prenom, $adress, $ville, $CP, $date_emb, $login, $mdp, $date);
     }
 }
 
