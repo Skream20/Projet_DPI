21f4bb65b267c29d5f985b2a8038a95b7d924c99
skream20 <yltcomptelt@gmail.com>
2024-02-15 16:46:22 +0100

    last_update_yann

--- page_web/web/remplir_id.php
+++ page_web/web/remplir_id.php
@@ -4,9 +4,9 @@ include 'db_connect.php';
 $cnxBDD = connexion();
 
 $km = $_GET['TxTKm'];
-$repas = 
-$nuit = 
-$
+$repas = $_GET['TxTRepas'];
+$nuit = $_GET['TxTNuit'];
+$etape = $_GET['TxTEtape'];
 
 function getNextFFR_ID()
 {
@@ -25,17 +25,23 @@ function getNextFFR_ID()
     return $next_id;
 }
 
-function rid($km)
+function rid($km, $repas, $nuit, $etape)
 {
     global $cnxBDD;
 
     $i = getNextFFR_ID();
 
     // Perform the loop only once
-    $r_id = "INSERT INTO ligne_frais_forfait(FFR_ID, FOR_ID, LIG_QTE) VALUES ($i, 'km', '$km')";
+    $r_id = "INSERT INTO ligne_frais_forfait(FFR_ID, FOR_ID, LIG_QTE)
+        VALUES 
+        ($i, 'km', '$km'),
+        ($i, 'NUI', '$nuit'),
+        ($i, 'ETP', '$etape'),
+        ($i, 'REP', '$repas')";
+
     echo "Sql : " . $r_id . "<br />";
 
     $res_id = $cnxBDD->query($r_id);
 }
 
-rid($km);
+rid($km, $repas, $nuit, $etape);
