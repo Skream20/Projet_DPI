084a8f3f9027a4cda12bcfebc5eb24a894e873ab
skream20 <yltcomptelt@gmail.com>
2024-02-15 17:05:54 +0100

    update_yann

--- page_web/web/traitement_fraie.php
+++ page_web/web/traitement_fraie.php
@@ -13,8 +13,24 @@ $nuitt = $GET_["Txthot"];
 $ETPt = $GET_["TxTetape"];
 $mois = $GET_["TxtMois"];
 $id = $_GET["TxTID"];
-
+$annee = $_GET["TxTAnnee"];
 #remplire FFR_ID
+function getNextFFR_ID()
+{
+    global $cnxBDD;
+
+    $query = "SELECT FFR_ID FROM ligne_frais_forfait ORDER BY FFR_ID DESC LIMIT 1";
+    $result = $cnxBDD->query($query);
+
+    if ($result && $result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $next_id = $row['FFR_ID'] + 1;
+    } else {
+        $next_id = 1;
+    }
+
+    return $next_id;
+}
 
 #calule fiche de fraie
 function calcule_remboursement($kmt, $repast, $nuitt, $ETPt)
@@ -40,12 +56,12 @@ function calcule_remboursement($kmt, $repast, $nuitt, $ETPt)
 
 //remplissage de la base de donnée fiche_frais
 #insert dans la BDD
-function fraie_visiteur($km, $repas, $nuit, $ETP, $id)
+function fraie_visiteur($km, $repas, $nuit, $ETP, $id, $annee)
 {
     calcule_remboursement($km, $repas, $nuit, $ETP);
 
     $sql = "INSERT INTO fiche_frais(FFR_ID, VIS_ID, ETA_ID, FFR_ANNEE, FFR_MOIS, FFR_MONTANT_VALIDE, FFR_NB_JUSTIFICATIF, FFR_DATE_MODIF)
-    VALUES ( '$id',)";
+    VALUES ( '$id','$annee','')";
 
     // Exécution de la requête
     echo "Sql : " . $sql . "<br />";
