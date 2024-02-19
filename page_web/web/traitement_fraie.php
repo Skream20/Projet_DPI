056ce4b62f63b793871ca66eb78f4dd96a87e2d6
skream20 <yltcomptelt@gmail.com>
2024-02-19 13:13:54 +0100

    last_update_yann

--- page_web/web/traitement_fraie.php
+++ page_web/web/traitement_fraie.php
@@ -56,12 +56,12 @@ function calcule_remboursement($kmt, $repast, $nuitt, $ETPt)
 
 //remplissage de la base de donnée fiche_frais
 #insert dans la BDD
-function fraie_visiteur($km, $repas, $nuit, $ETP, $id, $annee)
+function fraie_visiteur($km, $repas, $nuit, $ETP, $id, $mois, $annee)
 {
     calcule_remboursement($km, $repas, $nuit, $ETP);
 
     $sql = "INSERT INTO fiche_frais(FFR_ID, VIS_ID, ETA_ID, FFR_ANNEE, FFR_MOIS, FFR_MONTANT_VALIDE, FFR_NB_JUSTIFICATIF, FFR_DATE_MODIF)
-    VALUES ( '$id','$annee','')";
+    VALUES ( '$id','$annee','$mois')";
 
     // Exécution de la requête
     echo "Sql : " . $sql . "<br />";
@@ -69,7 +69,7 @@ function fraie_visiteur($km, $repas, $nuit, $ETP, $id, $annee)
 
 if (isset($_GET['BOvalider'])) {
     include 'remplir_id.php';
-    fraie_visiteur($km, $repas, $nuit, $ETP, $id);
+    fraie_visiteur($km, $repas, $nuit, $ETP, $id, $mois, $annee);
 }
 
 
